7113550590aceee933edcce4f8028e8d19c85b39
Nicolas Martyanoff <nicolas@n16f.net>
2023-04-03 17:20:19 +0200

    implement the uuid program

--- cmd/uuid/main.go
+++ cmd/uuid/main.go
@@ -1,9 +1,19 @@
 package main
 
-import "github.com/exograd/go-program"
+import (
+	"fmt"
+	"math"
+	"strconv"
+
+	"github.com/exograd/go-program"
+	"github.com/galdor/go-uuid"
+)
 
 func main() {
 	p := program.NewProgram("uuid", "utilities for the go-uuid library")
+
+	p.AddOption("v", "version", "version", "4",
+		"the version of the uuid to generate")
 
 	p.SetMain(mainCmd)
 
@@ -12,5 +22,17 @@ func main() {
 }
 
 func mainCmd(p *program.Program) {
-	// TODO
+	vs := p.OptionValue("version")
+	i64, err := strconv.ParseInt(vs, 10, 64)
+	if err != nil || i64 <= 0 || i64 > math.MaxInt32 {
+		p.Fatal("invalid version %q", vs)
+	}
+	v := uuid.Version(i64)
+
+	var id uuid.UUID
+	if err := id.Generate(v); err != nil {
+		p.Fatal("cannot generate uuid: %v", err)
+	}
+
+	fmt.Println(id.String())
 }
